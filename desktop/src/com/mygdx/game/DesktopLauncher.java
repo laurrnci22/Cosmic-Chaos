2593376ef2a7891b4648f303e6116e4d57eb758d
laurrnci22 <123271900+laurrnci22@users.noreply.github.com>
2023-06-15 19:47:27 +0200

    Add files via upload

---
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import shoot_em_up.ShootEmUP;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Cosmic Chaos");
+		config.setResizable(false);
+
+		config.setWindowedMode(1000, 1000);
+		new Lwjgl3Application(new ShootEmUP(), config);
+
+	}
+}
